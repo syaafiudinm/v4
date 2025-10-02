ba1392aa38404d83ead823ff68258e4daba07d4c
syaafiudinm <altafpasallo12@gmail.com>
2025-10-02 13:09:26 +0800

    building

---
+++ public/build/assets/input-error-BJrDeB8P.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-uPTyjskz.js";import{c as o}from"./app-logo-icon-CgSlR3SU.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
