c37f2b6b0ad866d607c919c301b2ea3fb2871267
syaafiudinm <altafpasallo12@gmail.com>
2025-10-02 22:00:24 +0800

    ready to build

--- public/build/assets/input-error-DymEj-oG.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-B3Yj_cCg.js";import{c as o}from"./app-logo-icon-BmJtU7Yl.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
