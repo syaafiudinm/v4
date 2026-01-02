ddec98730a3dcb745c83a47a596526d1f16e0156
syaafiudinm <altafpasallo12@gmail.com>
2026-01-02 21:11:20 +0800

    Build for prod

--- public/build/assets/input-error-BSQQ0bAP.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-Dpp0raZN.js";import{c as o}from"./app-logo-icon-BTI21LtV.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
